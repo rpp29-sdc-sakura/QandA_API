0bf304b1aebb074f95dff82f047486794883f325
Jaimie Diemer <pbandjaimie@gmail.com>
2021-09-28 18:52:48 -0700

    updated server index

--- server/index.js
+++ server/index.js
@@ -2,32 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
-const db = require('../database/index.js');
+const db = require('./../database/index.js');
+const controllers = require('./controllers.js');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(express.static(__dirname + '/../client/dist'));
 
-app.get('/test', (req, res) => {
-  // return db.updateData(num)
-  //   .then(() => {
-  //     db.fetchComplete(num)
-  //       .then((complete) => {
-  //         res.send(complete)
-  //       })
-  //   })
-  return db.callIt()
-    .then(() => {
-      console.log(Date.now())
-      res.sendStatus(200)
-    })
-  // return db.fetchComplete(4)
-  //   .then((complete) => {
-  //     res.send(complete)
-  //   })
-});
-
+app.get('/qa/questions', controllers.getData);
+app.put('/qa/questions/:question_id/helpful', controllers.likeQuestion);
+app.put('/qa/answers/:answer_id/helpful', controllers.likeAnswer);
+app.put('/qa/questions/:question_id/report', controllers.reportQuestion);
+app.put('/qa/answers/:answer_id/report', controllers.reportAnswer);
+app.post('/qa/questions', controllers.submitQuestion);
+app.post('/qa/questions/:question_id/answers', controllers.submitAnswer);
+app.post('/interactions', controllers.logInteraction);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
